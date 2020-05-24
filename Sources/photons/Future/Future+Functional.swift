1fd89fd296ff88b07969c6dfe037e8c33c43e44d
Jun Meng <joel.meng.au@gmail.com>
2020-05-24 12:11:13 +1000

    Extract function functions to seperate files

---
+++ Sources/photons/Future/Future+Functional.swift
@@ -0,0 +1,56 @@
+//
+//  Future+Functional.swift
+//  photons
+//
+//  Created by Jun Meng on 22/5/20.
+//
+
+import Foundation
+
+extension Future {
+    
+    // MARK: - Map
+    
+    func map<U>(f: @escaping (Value) -> U) -> Future<U> {
+        let newFuture = Future<U>()
+        onComplete(subscribeOn: currentContext) { value in
+            newFuture.resolve(with: f(value))
+        }
+        return newFuture
+    }
+    
+    public static func map<A, B>(future: Future<A>, f: @escaping (A) -> B) -> Future<B> {
+        future.map(f: f)
+    }
+    
+    // MARK: - FlatMap
+    
+    public func flatMap<U>(f: @escaping (Value) -> Future<U>) -> Future<U> {
+        let newFuture = Future<U>()
+        onComplete(subscribeOn: currentContext) { value in
+            f(value).onComplete(subscribeOn: currentContext) { valueU in
+                newFuture.resolve(with: valueU)
+            }
+        }
+        return newFuture
+    }
+    
+    public static func flatMap<A, B>(future: Future<A>, f: @escaping (A) -> Future<B>) -> Future<B> {
+        future.flatMap(f: f)
+    }
+    
+    // MARK: - Zip
+    
+    public func zip<A>(subscribeOn subscribeContext: @escaping ExectutionContext = backgroundContext,
+                       with anotherFuture: Future<A>) -> Future<(Value, A)> {
+        flatMap { (thisValue) -> Future<(Value, A)> in
+            anotherFuture.map { anotherValue in
+                (thisValue, anotherValue)
+            }
+        }
+    }
+    
+    public static func flatMap<A, B>(lhf: Future<A>, rhf: Future<B>) -> Future<(A, B)> {
+        lhf.zip(with: rhf)
+    }
+}
--- Sources/photons/Future/Future.swift
+++ Sources/photons/Future/Future.swift
@@ -105,56 +105,6 @@ public class Future<Value>: FutureType {
     }
 }
 
-// MARK: - Functional
-
-extension Future {
-    
-    // MARK: - Map
-    
-    func map<U>(f: @escaping (Value) -> U) -> Future<U> {
-        let newFuture = Future<U>()
-        onComplete(subscribeOn: currentContext) { value in
-            newFuture.resolve(with: f(value))
-        }
-        return newFuture
-    }
-    
-    public static func map<A, B>(future: Future<A>, f: @escaping (A) -> B) -> Future<B> {
-        future.map(f: f)
-    }
-    
-    // MARK: - FlatMap
-    
-    public func flatMap<U>(f: @escaping (Value) -> Future<U>) -> Future<U> {
-        let newFuture = Future<U>()
-        onComplete(subscribeOn: currentContext) { value in
-            f(value).onComplete(subscribeOn: currentContext) { valueU in
-                newFuture.resolve(with: valueU)
-            }
-        }
-        return newFuture
-    }
-    
-    public static func flatMap<A, B>(future: Future<A>, f: @escaping (A) -> Future<B>) -> Future<B> {
-        future.flatMap(f: f)
-    }
-    
-    // MARK: - Zip
-    
-    public func zip<A>(subscribeOn subscribeContext: @escaping ExectutionContext = backgroundContext,
-                       with anotherFuture: Future<A>) -> Future<(Value, A)> {
-        flatMap { (thisValue) -> Future<(Value, A)> in
-            anotherFuture.map { anotherValue in
-                (thisValue, anotherValue)
-            }
-        }
-    }
-    
-    public static func flatMap<A, B>(lhf: Future<A>, rhf: Future<B>) -> Future<(A, B)> {
-        lhf.zip(with: rhf)
-    }
-}
-
 precedencegroup infix0 {
     associativity: left
     higherThan: AssignmentPrecedence
